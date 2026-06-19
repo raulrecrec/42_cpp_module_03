cb921a162f7406b4f78f2e2fec7dab9c5a825b92
WSL HOME <raulrecrec@gmail.com>
2026-06-19 20:07:46 +0200

    Add ScavTrap class declaration
    - Created ScavTrap.hpp to declare the ScavTrap derived class.
    - Added public inheritance from ClapTrap.
    - Declared Orthodox Canonical Form methods, attack and guardGate.

---
+++ ex01/ScavTrap.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/19 18:37:18 by rexposit          #+#    #+#             */
+/*   Updated: 2026/06/19 18:53:22 by rexposit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+#include "ClapTrap.hpp"
+#include <string>
+
+class ScavTrap : public ClapTrap
+{
+	public:
+		ScavTrap(void);
+		ScavTrap(const std::string &name);
+		ScavTrap(const ScavTrap &other);
+		ScavTrap &operator=(const ScavTrap &other);
+		~ScavTrap(void);
+
+		void	attack(const std::string &target);
+		void	guardGate(void);
+};
+
+#endif
