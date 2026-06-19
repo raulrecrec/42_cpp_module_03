9c90d2236e515e1f8f0f0136e76c6f3609f36e5d
WSL HOME <raulrecrec@gmail.com>
2026-06-19 14:17:05 +0200

    Add ClapTrap class declaration
    - Created ClapTrap.hpp to declare the ClapTrap class.
    - Added private attributes for name, hit points, energy points and attack damage.
    - Declared Orthodox Canonical Form methods and ClapTrap actions.

---
+++ ex00/ClapTrap.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/19 13:15:48 by rexposit          #+#    #+#             */
+/*   Updated: 2026/06/19 13:54:13 by rexposit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+#include <string>
+
+class ClapTrap
+{
+	private:
+	std::string		name;
+	unsigned int	hit_points;
+	unsigned int	energy_points;
+	unsigned int	attack_damage;
+
+	public:
+	ClapTrap(void);
+	ClapTrap(const std::string &name);
+	ClapTrap(const ClapTrap &other);
+	ClapTrap &operator=(const ClapTrap &other);
+	~ClapTrap(void);
+
+	void	attack(const std::string &target);
+	void	takeDamage(unsigned int amount);
+	void	beRepaired(unsigned int amount);
+};
+
+#endif
